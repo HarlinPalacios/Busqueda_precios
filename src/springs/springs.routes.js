7915dd0b3e6a020983dab4bcb7b02ea2d9be2e91
palacios <hpalacios-2020586@kinal.edu.gt>
2025-10-08 11:51:05 -0600

    correciones 4

--- src/springs/springs.routes.js
+++ src/springs/springs.routes.js
@@ -1,11 +1,21 @@
-import { Router } from "express"
-import { createSprings, getSprings, updateSpringsById, deleteSprings } from "../springs/springs.conroller.js"
+import { Router } from "express";
+import multer from "multer";
+import {
+  createSprings,
+  getSprings,
+  updateSpringsById,
+  deleteSprings,
+} from "../springs/springs.conroller.js";
 
-const router = Router()
+const router = Router();
 
-router.post("/createSprings", createSprings);
-router.get("/getSprings", getSprings)
-router.put("/updateSprings/:id", updateSpringsById)
-router.delete("/deleteSprings/:id", deleteSprings)
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
-export default router; 
+router.post("/createSprings", upload.single("imagen"), createSprings);
+
+router.get("/getSprings", getSprings);
+router.put("/updateSprings/:id", updateSpringsById);
+router.delete("/deleteSprings/:id", deleteSprings);
+
+export default router;
